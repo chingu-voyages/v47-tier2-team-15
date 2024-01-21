f76a41670ecd831695a0a015e0df94b7fd250a6d
Ana Popovic <franjetina@gmail.com>
2024-01-21 21:13:47 +0100

    input focus styled

--- v47-team-15/client/src/components/display_data/Search.jsx
+++ v47-team-15/client/src/components/display_data/Search.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import { useState } from "react";
 
 function Search() {
+    const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible((prev) => !prev);
+  };
+
   return (
     <>
-      <div className="flex justify-center bg-[#1A183E] py-6">
-        <div>
-          <input type="text" placeholder="Search cryptocurrencies" className='rounded py-2 px-6' />
-          <i className='bx bx-search' color="white"></i>
-        </div>
+      <div className="flex justify-center items-center bg-[#1A183E] gap-2 py-6">
+      <div
+        className={`relative ${isVisible ? 'w-60' : 'w-0'} overflow-hidden transition-width duration-500`}
+      >
+        <input
+          type="text"
+          placeholder="Search cryptocurrencies"
+          className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-[#311B3D] focus:ring-[#40254f] block w-full rounded-md sm:text-sm focus:ring-1"
+        />
+      </div>
+          <i className="bx bx-search text-white text-xl cursor-pointer"
+        onClick={toggleVisibility}></i>
       </div>
     </>
   );
