ef7d4fc43a33b0e2545ccc55304b4bcb505c5bdf
Ana Popovic <franjetina@gmail.com>
2024-01-21 19:47:05 +0100

    boxicons added

--- v47-team-15/client/src/components/display_data/Search.jsx
+++ v47-team-15/client/src/components/display_data/Search.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 function Search() {
   return (
-    <div>Search</div>
-  )
+    <>
+      <div className="flex justify-center bg-[#1A183E] py-6">
+        <div>
+          <input type="text" placeholder="Search cryptocurrencies" className='rounded py-2 px-6' />
+          <i className='bx bx-search' color="white"></i>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Search
+export default Search;
