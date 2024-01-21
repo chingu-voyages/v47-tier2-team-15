c303481f578ab2f14353073baa6fe8b6e55d6f7c
Ana Popovic <franjetina@gmail.com>
2024-01-21 14:43:24 +0100

    info cards hidden on small screens

--- v47-team-15/client/src/pages/DisplayData.jsx
+++ v47-team-15/client/src/pages/DisplayData.jsx
@@ -1,11 +1,13 @@
 import Header from '../components/display_data/Header.jsx';
 import InfoCards from '../components/display_data/InfoCards.jsx';
+import Search from '../components/display_data/Search.jsx';
 
 function DisplayData() {
   return (
     <>
       <Header />
       <InfoCards />
+      <Search />
     </>
   )
 }
