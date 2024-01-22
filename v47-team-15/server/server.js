68039189a7b59a838c4f3a4e11fa2e17b5effe1b
Ana Popovic <franjetina@gmail.com>
2024-01-22 11:24:57 +0100

    cors and headers added

--- v47-team-15/server/server.js
+++ v47-team-15/server/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const currenciesRoute = require('./routes/currenciesRoute');
 const app = express();
+const cors = require('cors');
+const currenciesRoute = require('./routes/currenciesRoute');
 const bodyParser = require('body-parser');
+
 app.use(express.json());
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
+
+app.use('/api/currencies', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use(bodyParser.json());
 
 app.use('/api/currencies', currenciesRoute);
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
