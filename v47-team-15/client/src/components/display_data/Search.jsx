521c71b2a43144c8596e166af31cb7c7d5f82124
Ana Popovic <franjetina@gmail.com>
2024-01-22 13:31:56 +0100

    search functionality added

--- v47-team-15/client/src/components/display_data/Search.jsx
+++ v47-team-15/client/src/components/display_data/Search.jsx
@@ -1,29 +1,43 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
-function Search() {
-    const [isVisible, setIsVisible] = useState(false);
+function Search({ onSearch }) {
+  const [searchInput, setSearchInput] = useState("");
 
-  const toggleVisibility = () => {
-    setIsVisible((prev) => !prev);
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleClick = () => {
+    console.log('Button clicked');
+    onSearch(searchInput);
   };
 
   return (
     <>
       <div className="flex justify-center items-center bg-[#1A183E] gap-2 py-6">
-      <div
-        className={`relative ${isVisible ? 'w-60' : 'w-0'} overflow-hidden transition-width duration-500`}
-      >
-        <input
-          type="text"
-          placeholder="Search cryptocurrencies"
-          className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-[#311B3D] focus:ring-[#40254f] block w-full rounded-md sm:text-sm focus:ring-1"
-        />
-      </div>
-          <i className="bx bx-search text-white text-xl cursor-pointer"
-        onClick={toggleVisibility}></i>
+        <div className="w-60">
+          <input
+            type="text"
+            placeholder="Search cryptocurrencies"
+            className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-[#311B3D] focus:ring-[#40254f] block w-full rounded-md sm:text-sm focus:ring-1"
+            value={searchInput}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button
+          className="bg-[#1A183E] text-white text-xl cursor-pointer"
+          onClick={handleClick}
+        >
+          <i className="bx bx-search"></i>
+        </button>
       </div>
     </>
   );
 }
+
+Search.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+};
 
 export default Search;
