04c779478025c12775855909a6d56014ab96dacb
Ana Popovic <franjetina@gmail.com>
2024-01-23 12:48:38 +0100

    api data added, format numbers and changing color functions added

--- v47-team-15/client/src/components/display_data/InfoCards.jsx
+++ v47-team-15/client/src/components/display_data/InfoCards.jsx
@@ -1,4 +1,26 @@
+import useFetch from '../useFetch';
+
 function InfoCards() {
+  const { data } = useFetch('http://localhost:3003/api/global');
+
+  if (!data || !data[0]) {
+    return <div>No data available</div>;
+  }
+
+  const formatNumber = (data, name, decimal) => {
+    const value = data[0]?.[name];
+
+    if (value !== undefined && value !== null) {
+      return value.toFixed(decimal).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    } else {
+      return 'N/A';
+    }
+  };
+  // Error fetching global data: Socket connection timeout
+  function getColor(number) {
+    return number >= 0 ? 'text-[#34B349]' : 'text-[#F02934]';
+  }
+
   return (
     <>
       <div className="w-full flex flex-col justify-center items-center bg-[#1A183E] text-white p-4">
@@ -6,30 +28,30 @@ function InfoCards() {
           <p className="text-2xl py-4">All Cryptocurrencies</p>
           <p className="pb-4">View a full list of active cryptocurrencies</p>
         </div>
-        <div className="hidden md:flex flex-row items-center justify-center gap-2">
+        <div className="hidden md:flex flex-row items-center justify-center gap-4">
           <div className="min-w-[15rem] bg-[#1D2A41] rounded p-4">
             <p className="pb-4">Market Cap</p>
             <div className="flex flex-row gap-4">
-              <span>$1,757,512,361,161</span>
-              <span className="bg-[#1F3A43] rounded text-[#34B349] px-1">
-                1.00%
+              <span>{formatNumber(data, 'total_mcap', 2)}</span>
+              <span className={`rounded bg-[#1F3A43] px-1 ${getColor(data[0].mcap_change)}`}>
+              {data[0].mcap_change}%
               </span>
             </div>
           </div>
           <div className="min-w-[15rem] bg-[#1D2A41] rounded p-4">
             <p className="pb-4">Volume 24h</p>
             <div className="flex flex-row gap-4">
-              <span>$80,107,052,528</span>
-              <span className="bg-[#1F3A43] rounded text-[#34B349] px-1">
-                37.1%
+              <span>{formatNumber(data, 'total_volume', 2)}</span>
+              <span className={`bg-[#1F3A43] rounded px-1 ${getColor(data[0].volume_change)}`}>
+              {data[0].volume_change}%
               </span>
             </div>
           </div>
           <div className="min-w-[15rem] bg-[#311B3D] rounded p-4">
             <p className="pb-4">BTC Dominance</p>
             <div className="flex flex-row gap-4">
-              <span>47.8%</span>
-              <span className="bg-[#461E3C] rounded text-[#F02934] px-1">
+              <span>{data[0].btc_d}%</span>
+              <span className="bg-[#461E3C] rounded px-1">
                 0.31%
               </span>
             </div>
