9562881667594a4d227e5df2e7c48c4b2f9776c9
Ana Popovic <franjetina@gmail.com>
2024-01-24 14:23:57 +0100

    fix: text small

--- v47-team-15/client/src/components/display_data/Table.jsx
+++ v47-team-15/client/src/components/display_data/Table.jsx
@@ -4,10 +4,9 @@ import { getColor } from '../Helpers';
 function Table({ data, filter }) {
   const displayData = filter.length > 0 ? filter : data;
 
-
   return (
     <>
-    <div className='bg-[#1A183E] py-8'>
+      <div className='bg-[#1A183E] py-8'>
       <table className="min-w-3/5 mx-auto divide-y divide-gray-200 rounded-md">
         <div className='overflow-y-auto max-h-[35rem]'>
         <thead className="sticky top-0 bg-gray-50 z-10">
@@ -48,7 +47,7 @@ function Table({ data, filter }) {
             
           </tr>
         </thead>
-        <tbody className="bg-[#24224B] text-white divide-y divide-gray-200">
+        <tbody className="bg-[#24224B] text-white text-xs divide-y divide-gray-200">
           {displayData.map((coin, index) => (
             <tr key={index}>
               <td className="px-6 py-4 whitespace-nowrap">{coin.rank}</td>
@@ -68,13 +67,57 @@ function Table({ data, filter }) {
         </div>
       </table>
     </div>
+      {/* <div className="bg-[#1A183E] py-8">
+        <table className="text-left w-4/5 divide-y divide-gray-200 mx-auto">
+          <thead className="bg-gray-50 flex text-gray-700 w-full">
+            <tr className="flex w-full">
+              <th className="p-1 w-1/4">Rank</th>
+              <th className="p-1 w-1/4">Name</th>
+              <th className="p-1 w-1/4">Symbol</th>
+              <th className="p-1 w-1/4">Market Cap (USD)</th>
+              <th className="p-1 w-1/4">Price</th>
+              <th className="p-1 w-1/4">1h</th>
+              <th className="p-1 w-1/4">24h</th>
+              <th className="p-1 w-1/4">7d</th>
+              <th className="p-1 w-1/4">Circulating Supply</th>
+              <th className="p-1 w-1/4">Total Supply</th>
+              <th className="p-1 w-1/4">Market Cap</th>
+            </tr>
+          </thead>
+          <tbody className="bg-[#24224B] divide-y divide-gray-200 text-white flex flex-col items-center justify-between overflow-y-scroll w-full h-[50vh]">
+            {displayData.map((coin, index) => (
+              <tr key={index} className="flex w-full text-xs mb-4">
+                <td className="p-4 w-1/4">{coin.rank}</td>
+                <td className="p-4 w-1/4">{coin.name}</td>
+                <td className="p-4 w-1/4">{coin.symbol}</td>
+                <td className="p-4 w-1/4">{coin.market_cap_usd}</td>
+                <td className="p-4 w-1/4">{coin.price_usd}</td>
+                <td className={`p-4 w-1/4 ${getColor(coin.percent_change_1h)}`}>
+                  {coin.percent_change_1h}
+                </td>
+                <td
+                  className={`p-4 w-1/4 ${getColor(coin.percent_change_24h)}`}
+                >
+                  {coin.percent_change_24h}
+                </td>
+                <td className={`p-4 w-1/4 ${getColor(coin.percent_change_7d)}`}>
+                  {coin.percent_change_7d}
+                </td>
+                <td className="p-4 w-1/4">{coin.csupply}</td>
+                <td className="p-4 w-1/4">{coin.tsupply}</td>
+                <td className="p-4 w-1/4">{coin.market_cap_usd}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div> */}
     </>
   );
 }
 
 Table.propTypes = {
   data: PropTypes.array.isRequired,
-  filter: PropTypes.array.isRequired
+  filter: PropTypes.array.isRequired,
 };
 
 export default Table;
