547587e7a36a3b0f05e07a55e5ab8c899ad7ec2e
Ana Popovic <franjetina@gmail.com>
2024-01-24 16:05:39 +0100

    fix: footer content changed

--- v47-team-15/client/src/components/display_data/Footer.jsx
+++ v47-team-15/client/src/components/display_data/Footer.jsx
@@ -1,24 +1,17 @@
 const Footer = () => {
   return (
     <>
-      <div className="flex justify-center mt-4 md:mt-0 md:font-medium py-1 md:py-0 transition-all delay-150 duration-300 overflow-hidden px-6 bg-[#1A183E] text-white">
-        <a
-          href="#"
-          target="_blank"
-          rel="noreferrer"
-          className="text-lg text-white block pl-3 pr-4 py-2 rounded transition ease-in-out delay-150 hover:scale-125 cursor-pointer"
-        >
-          LinkedIn
-        </a>
+      <div className="flex flex-row justify-center mt-4 md:mt-0 md:font-medium py-1 md:py-0 transition-all delay-150 duration-300 overflow-hidden px-6 bg-[#1A183E] text-white text-xs">
+          &copy; 2024 | Chingu |
         <a
           href="https://github.com/chingu-voyages/v47-tier2-team-15"
           target="_blank"
           rel="noreferrer"
-          className="text-lg text-white block pl-3 pr-4 py-2 rounded transition ease-in-out delay-150 hover:scale-125 cursor-pointer"
+          className=" text-white mx-1 transition ease-in-out delay-150 hover:scale-125 cursor-pointer"
         >
           Github
         </a>
-      </div>
+        </div>
     </>
   );
 };
