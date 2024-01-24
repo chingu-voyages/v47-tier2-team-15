f925b017be6e1eba1340d94da9f0f73f0819f992
Isabell <isabellhepburn@outlook.com>
2024-01-24 17:44:15 +0100

    feat:connect with mongoDB

--- v47-team-15/server/controllers/cacheController.js
+++ v47-team-15/server/controllers/cacheController.js
@@ -3,6 +3,13 @@ const mongoose = require('mongoose');
 // import Currency in the file models/currency.js
 const Currency = require('../models/currency');
 
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+
 const getCachedCurrencies = async (start = 0, limit = 100) => {
   try {
     // Assuming Currency is the Mongoose model for your collection
