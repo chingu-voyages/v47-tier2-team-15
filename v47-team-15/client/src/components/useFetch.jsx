5eab15231133cc907752e1926e84ddd67e1ee592
Ana Popovic <franjetina@gmail.com>
2024-01-25 12:28:40 +0100

    feat: spinner added

--- v47-team-15/client/src/components/useFetch.jsx
+++ v47-team-15/client/src/components/useFetch.jsx
@@ -4,10 +4,13 @@ import axios from 'axios';
 function useFetch(url) {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true);
+
         const response = await axios.get(url, {
           timeout: 15000,
           withCredentials: true,
@@ -19,13 +22,15 @@ function useFetch(url) {
       } catch (error) {
         console.error('Error fetching data:', error.message);
         setError(error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchData();
   }, [url]);
 
-  return { data, error };
+  return { data, error, isLoading };
 }
 
 export default useFetch;
--- v47-team-15/client/src/pages/DisplayData.jsx
+++ v47-team-15/client/src/pages/DisplayData.jsx
@@ -6,21 +6,23 @@ import Table from '../components/display_data/Table.jsx';
 import useFetch from '../components/useFetch.jsx';
 import Footer from '../components/display_data/Footer.jsx';
 import Pagination from '../components/display_data/Pagination.jsx';
+import Spinner from '../components/display_data/Spinner.jsx';
 
 function DisplayData() {
-  const { data, error } = useFetch('http://localhost:3003/api/currencies');
-  const [ filteredData, setFilteredData ] = useState([]);
+  const { data, error, isLoading } = useFetch(
+    'http://localhost:3003/api/currencies',
+  );
+  const [filteredData, setFilteredData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10;
- 
 
   const handleSearch = (searchInput) => {
     if (searchInput) {
-      const filteredResults = data.filter(item =>
-        item.name.toLowerCase().includes(searchInput.toLowerCase())
+      const filteredResults = data.filter((item) =>
+        item.name.toLowerCase().includes(searchInput.toLowerCase()),
       );
       setFilteredData(filteredResults);
-      console.log("search results:", filteredResults)
+      console.log('search results:', filteredResults);
     } else {
       setFilteredData(data);
     }
@@ -37,11 +39,27 @@ function DisplayData() {
         <div>Error fetching data: {error.message}</div>
       ) : (
         <>
-          <InfoCards />
-          <Search onSearch={handleSearch} /> 
-          <Table data={data} filter={filteredData} currentPage={currentPage} itemsPerPage={itemsPerPage} />
-          <Pagination totalItems={filteredData.length || data.length} itemsPerPage={itemsPerPage} currentPage={currentPage} onPageChange={handlePageChange} />
-          <Footer />
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <>
+              <InfoCards />
+              <Search onSearch={handleSearch} />
+              <Table
+                data={data}
+                filter={filteredData}
+                currentPage={currentPage}
+                itemsPerPage={itemsPerPage}
+              />
+              <Pagination
+                totalItems={filteredData.length || data.length}
+                itemsPerPage={itemsPerPage}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+              />
+              <Footer />
+            </>
+          )}
         </>
       )}
     </>
