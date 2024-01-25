07e2858b5d15d93f1380adfe798ee34f7f3ca096
Ana Popovic <franjetina@gmail.com>
2024-01-25 10:36:53 +0100

    feat: made table numbers floating

--- v47-team-15/client/src/components/Helpers.jsx
+++ v47-team-15/client/src/components/Helpers.jsx
@@ -8,6 +8,15 @@ export const formatNumber = (data, name, decimal) => {
   }
 };
 
+export const formatTableNumbers = (data) => {
+  const value = parseFloat(data);
+  if (!isNaN(value)) {
+    return (value + 2).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  } else {
+    return "N/A";
+  }
+};
+
 export function getColor(number) {
   return number >= 0 ? 'text-[#34B349]' : 'text-[#F02934]';
 }
--- v47-team-15/client/src/components/display_data/Table.jsx
+++ v47-team-15/client/src/components/display_data/Table.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
-import { getColor } from '../Helpers';
+import { getColor, formatTableNumbers } from '../Helpers';
 
-function Table({ data, filter, currentPage, itemsPerPage  }) {
+function Table({ data, filter, currentPage, itemsPerPage }) {
   const displayData = filter.length > 0 ? filter : data;
 
   const startIndex = (currentPage - 1) * itemsPerPage;
@@ -11,107 +11,82 @@ function Table({ data, filter, currentPage, itemsPerPage  }) {
 
   return (
     <>
-      <div className='bg-[#1A183E] py-8'>
-      <table className="min-w-3/5 mx-auto divide-y divide-gray-200 rounded-md">
-        {/* <div className='overflow-y-auto max-h-[35rem]'> */}
-        <thead className="sticky top-0 bg-gray-50 z-10">
-          <tr>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Rank
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Name
-            </th>
-            <th className="hidden sm:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Symbol
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Price
-            </th>
-            <th className="hidden md:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              1h
-            </th>
-            <th className="hidden md:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              24h
-            </th>
-            <th className="hidden md:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              7d
-            </th>
-            <th className="hidden lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Circulating Supply
-            </th>
-            <th className="hidden lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Total Supply
-            </th>
-            <th className="hidden sm:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Market Cap
-            </th>
-            
-          </tr>
-        </thead>
-        <tbody className="bg-[#24224B] text-white text-xs divide-y divide-gray-200">
-          {pageData.map((coin, index) => (
-            <tr key={index}>
-              <td className="px-6 py-4 whitespace-nowrap">{coin.rank}</td>
-              <td className="px-6 py-4 whitespace-nowrap">{coin.name}</td>
-              <td className="hidden sm:table-cell px-6 py-4 whitespace-nowrap">{coin.symbol}</td>
-              <td className="px-6 py-4 whitespace-nowrap">{coin.price_usd}$</td>
-              <td className={`hidden md:table-cell px-6 py-4 whitespace-nowrap ${getColor(coin.percent_change_1h)}`}>{coin.percent_change_1h}%</td>
-              <td className={`hidden md:table-cell px-6 py-4 whitespace-nowrap ${getColor(coin.percent_change_7d)}`}>{coin.percent_change_7d}%</td>
-              <td className={`hidden md:table-cell px-6 py-4 whitespace-nowrap ${getColor(coin.percent_change_24h)}`}>{coin.percent_change_24h}%</td>
-              <td className="hidden lg:table-cell px-6 py-4 whitespace-nowrap">{coin.csupply}$</td>
-              <td className="hidden lg:table-cell px-6 py-4 whitespace-nowrap">{coin.tsupply}$</td>
-              <td className="hidden sm:table-cell px-6 py-4 whitespace-nowrap">{coin.market_cap_usd}$</td>
-            </tr>
-          ))}
-        </tbody>
-        {/* </div> */}
-      </table>
-    </div>
-      {/* <div className="bg-[#1A183E] py-8">
-        <table className="text-left w-4/5 divide-y divide-gray-200 mx-auto">
-          <thead className="bg-gray-50 flex text-gray-700 w-full">
-            <tr className="flex w-full">
-              <th className="p-1 w-1/4">Rank</th>
-              <th className="p-1 w-1/4">Name</th>
-              <th className="p-1 w-1/4">Symbol</th>
-              <th className="p-1 w-1/4">Market Cap (USD)</th>
-              <th className="p-1 w-1/4">Price</th>
-              <th className="p-1 w-1/4">1h</th>
-              <th className="p-1 w-1/4">24h</th>
-              <th className="p-1 w-1/4">7d</th>
-              <th className="p-1 w-1/4">Circulating Supply</th>
-              <th className="p-1 w-1/4">Total Supply</th>
-              <th className="p-1 w-1/4">Market Cap</th>
+      <div className="bg-[#1A183E] py-8">
+        <table className="min-w-3/5 mx-auto divide-y divide-gray-200 rounded-md">
+          <thead className="sticky top-0 bg-gray-50 z-10">
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Rank
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Name
+              </th>
+              <th className="hidden sm:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Symbol
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Price
+              </th>
+              <th className="hidden md:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                1h
+              </th>
+              <th className="hidden md:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                24h
+              </th>
+              <th className="hidden md:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                7d
+              </th>
+              <th className="hidden lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Circulating Supply
+              </th>
+              <th className="hidden lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Total Supply
+              </th>
+              <th className="hidden sm:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Market Cap
+              </th>
             </tr>
           </thead>
-          <tbody className="bg-[#24224B] divide-y divide-gray-200 text-white flex flex-col items-center justify-between overflow-y-scroll w-full h-[50vh]">
-            {displayData.map((coin, index) => (
-              <tr key={index} className="flex w-full text-xs mb-4">
-                <td className="p-4 w-1/4">{coin.rank}</td>
-                <td className="p-4 w-1/4">{coin.name}</td>
-                <td className="p-4 w-1/4">{coin.symbol}</td>
-                <td className="p-4 w-1/4">{coin.market_cap_usd}</td>
-                <td className="p-4 w-1/4">{coin.price_usd}</td>
-                <td className={`p-4 w-1/4 ${getColor(coin.percent_change_1h)}`}>
-                  {coin.percent_change_1h}
+          <tbody className="bg-[#24224B] text-white text-xs divide-y divide-gray-200">
+            {pageData.map((coin, index) => (
+              <tr key={index}>
+                <td className="px-6 py-4 whitespace-nowrap">{coin.rank}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{coin.name}</td>
+                <td className="hidden sm:table-cell px-6 py-4 whitespace-nowrap">
+                  {coin.symbol}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {formatTableNumbers(coin.price_usd)}$
                 </td>
                 <td
-                  className={`p-4 w-1/4 ${getColor(coin.percent_change_24h)}`}
+                  className={`hidden md:table-cell px-6 py-4 whitespace-nowrap ${getColor(coin.percent_change_1h)}`}
                 >
-                  {coin.percent_change_24h}
+                  {formatTableNumbers(coin.percent_change_1h)}%
                 </td>
-                <td className={`p-4 w-1/4 ${getColor(coin.percent_change_7d)}`}>
-                  {coin.percent_change_7d}
+                <td
+                  className={`hidden md:table-cell px-6 py-4 whitespace-nowrap ${getColor(coin.percent_change_7d)}`}
+                >
+                  {formatTableNumbers(coin.percent_change_7d)}%
                 </td>
-                <td className="p-4 w-1/4">{coin.csupply}</td>
-                <td className="p-4 w-1/4">{coin.tsupply}</td>
-                <td className="p-4 w-1/4">{coin.market_cap_usd}</td>
+                <td
+                  className={`hidden md:table-cell px-6 py-4 whitespace-nowrap ${getColor(coin.percent_change_24h)}`}
+                >
+                  {formatTableNumbers(coin.percent_change_24h)}%
+                </td>
+                <td className="hidden lg:table-cell px-6 py-4 whitespace-nowrap">
+                  {formatTableNumbers(coin.csupply)}$
+                </td>
+                <td className="hidden lg:table-cell px-6 py-4 whitespace-nowrap">
+                  {formatTableNumbers(coin.tsupply)}$
+                </td>
+                <td className="hidden sm:table-cell px-6 py-4 whitespace-nowrap">
+                  {formatTableNumbers(coin.market_cap_usd)}$
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
-      </div> */}
+      </div>
     </>
   );
 }
