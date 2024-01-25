e6f3eb392a83aaf181d1c26a4da198cb4cd27a48
Ana Popovic <franjetina@gmail.com>
2024-01-25 09:40:53 +0100

    feat: enter key enabled to display results

--- v47-team-15/client/src/components/display_data/Search.jsx
+++ v47-team-15/client/src/components/display_data/Search.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 function Search({ onSearch }) {
-  const [searchInput, setSearchInput] = useState("");
+  const [searchInput, setSearchInput] = useState('');
 
   const handleInputChange = (e) => {
     setSearchInput(e.target.value);
@@ -11,27 +11,32 @@ function Search({ onSearch }) {
   const handleClick = () => {
     console.log('Button clicked');
     onSearch(searchInput);
-    setSearchInput("");
+    setSearchInput('');
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      console.log('Enter key pressed');
+      onSearch(searchInput);
+      setSearchInput('');
+    }
   };
 
   return (
     <>
       <div className="flex justify-center items-center bg-[#1A183E] gap-2 py-6">
-        <div className="w-60">
-          <input
-            type="text"
-            placeholder="Search cryptocurrencies"
-            className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-[#311B3D] focus:ring-[#40254f] block w-full rounded-md sm:text-sm focus:ring-1"
-            value={searchInput}
-            onChange={handleInputChange}
-          />
-        </div>
-        <button
-          className="bg-[#1A183E] text-white text-xl cursor-pointer"
+        <input
+          type="text"
+          placeholder="Search cryptocurrencies"
+          className="w-1/2 md:w-1/4 mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-[#311B3D] focus:ring-[#40254f] block rounded-md sm:text-sm focus:ring-1"
+          value={searchInput}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        />
+        <i
           onClick={handleClick}
-        >
-          <i className="bx bx-search"></i>
-        </button>
+          className="bx bx-search text-white text-xl cursor-pointer"
+        ></i>
       </div>
     </>
   );
