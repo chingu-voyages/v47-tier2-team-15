8dc34c266d5b58cca8c41a9927b71bbb869ce35c
Ana Popovic <franjetina@gmail.com>
2024-01-25 09:29:46 +0100

    fix: padding added

--- v47-team-15/client/src/components/display_data/Footer.jsx
+++ v47-team-15/client/src/components/display_data/Footer.jsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
     <>
-      <div className="flex flex-row justify-center mt-4 md:mt-0 md:font-medium py-1 md:py-0 transition-all delay-150 duration-300 overflow-hidden px-6 bg-[#1A183E] text-white text-xs">
+      <div className="flex flex-row justify-center py-4 md:mt-0 md:font-medium py-1 md:py-0 transition-all delay-150 duration-300 overflow-hidden px-6 bg-[#1A183E] text-white font-thin text-xs">
           &copy; 2024 | Chingu |
         <a
           href="https://github.com/chingu-voyages/v47-tier2-team-15"
           target="_blank"
           rel="noreferrer"
-          className=" text-white mx-1 transition ease-in-out delay-150 hover:scale-125 cursor-pointer"
+          className="mx-1 transition ease-in-out delay-150 hover:scale-125 cursor-pointer"
         >
           Github
         </a>
