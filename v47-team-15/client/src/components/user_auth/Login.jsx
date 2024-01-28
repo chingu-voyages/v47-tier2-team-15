61ae1d1b5393d2f515f2afa5a6d1c66f3eac1133
Ana Popovic <franjetina@gmail.com>
2024-01-28 15:53:47 +0100

    feat: login form set

--- v47-team-15/client/src/components/user_auth/Login.jsx
+++ v47-team-15/client/src/components/user_auth/Login.jsx
@@ -1,12 +1,53 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-// import axios from 'axios';
+import axios from 'axios';
+import { useContext } from 'react';
+import { UserContext } from '../userContext';
 
 function Login({ closeModal, loginModalOpen, setRegistrationModalOpen }) {
 
+  const { setUsername, setSuccessMessage } = useContext(UserContext);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        'http://localhost:3003/auth/login',
+        formData,
+        {
+          withCredentials: true,
+          responseType: 'json',
+        },
+      );
+      setUsername(response.data.user.username);
+      setSuccessMessage(true);
+      console.log('Login data:', response.data.user );
+      closeModal();
+    } catch (error) {
+      console.error('Login error:', error.message);
+      setError('Incorrect email or password. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const navigateToSignup = () => {
-    closeModal('login'); 
-    setRegistrationModalOpen(true); 
+    closeModal(); 
+    setRegistrationModalOpen(true);
   };
     
   return (
@@ -30,59 +71,62 @@ function Login({ closeModal, loginModalOpen, setRegistrationModalOpen }) {
                 x
               </button>
             </div>
-            <form className="w-full flex flex-col p-2">
-             
-              <div className="w-full mb-4 flex flex-col justify-center items-center">
-                <label
-                  htmlFor="fullName"
-                  className="self-start px-16 text-white font-medium mb-1"
-                >
-                  Email
-                </label>
-                <input
-                  type="text"
-                  id="email"
-                  name="email"
-                //   value={formData.email}
-                //   onChange={handleChange}
-                  className="w-4/5 text-black px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                  required
-                />
-              </div>
+            <form className="w-full flex flex-col p-2" onSubmit={handleSubmit}>
+          <div className="w-full mb-4 flex flex-col justify-center items-center">
+            <label
+              htmlFor="email"
+              className="self-start px-16 text-white font-medium mb-1"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-4/5 text-black px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              required
+            />
+          </div>
 
-              <div className="w-full mb-4 flex flex-col justify-center items-center">
-                <label
-                  htmlFor="password"
-                  className="self-start px-16 text-white font-medium mb-1"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                //   value={formData.password}
-                //   onChange={handleChange}
-                  className="w-4/5 text-black px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-                  required
-                />
-              </div>
+          <div className="w-full mb-4 flex flex-col justify-center items-center">
+            <label
+              htmlFor="password"
+              className="self-start px-16 text-white font-medium mb-1"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="w-4/5 text-black px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              required
+            />
+          </div>
 
-              <div className="w-full mb-4 flex flex-col justify-center items-center">
-                <button
-                  type="submit"
-                  className="w-1/4 bg-[#00A83E] text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-                  // disabled={loading}
-                >
-                  Login
-                </button>
-                
-              </div>
-              <small className="text-white text-center">
-                Don't have an account? 
-                <button type="button" onClick={navigateToSignup}  className="text-blue-400 cursor-pointer px-1"> Sign up</button>
-              </small>
-            </form>
+          <div className="w-full mb-4 flex flex-col justify-center items-center">
+            <button
+              type="submit"
+              className="w-1/4 bg-[#00A83E] text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+              disabled={loading}
+            >
+              {loading ? 'Logging In...' : 'Login'}
+            </button>
+            
+          </div>
+          <small className="text-white text-center">
+            Don't have an account? 
+            <button type="button" onClick={navigateToSignup} className="text-blue-400 cursor-pointer px-1"> Sign up</button>
+          </small>
+
+          {error && (
+            <div className="text-red-500 mt-2">{error}</div>
+          )}
+        </form>
           </div>
         </div>
         {/* </div> */}
