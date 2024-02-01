a299bbc1804672ec72916bee57c7e1a92a855cda
Ana Popovic <franjetina@gmail.com>
2024-02-01 17:19:16 +0100

    fix: style fixed

--- v47-team-15/client/src/components/user_profile/AddCoins.jsx
+++ v47-team-15/client/src/components/user_profile/AddCoins.jsx
@@ -4,7 +4,7 @@ function AddCoins() {
   return (
     <>
         <div className='bg-[#1A183E] flex flex-row justify-around py-6'>
-            <div className='flex flex-col justify-center items-center text-white gap-4'>
+            <div className='flex flex-col justify-center items-center text-white gap-4 bg-[#24224B] p-6'>
                 <h1 className='text-2xl'>Add favorite coins</h1>
                 <small>Track your favorite cryptocurrencies easily.</small>
                 <input></input>
--- v47-team-15/client/src/components/user_profile/UserInfo.jsx
+++ v47-team-15/client/src/components/user_profile/UserInfo.jsx
@@ -11,7 +11,7 @@ function UserInfo() {
   return (
     <>
       <div className="w-full bg-[#1A183E] text-white h-1/2 p-16 flex flex-col justify-center items-center gap-4">
-        <div className='text-2xl'>Hello {username}!</div>
+        <div className='text-2xl'>Hello, {username}!</div>
         <div className="w-28 h-28 text-center rounded-full bg-[#24224B] uppercase relative">
           <span className='text-3xl absolute top-8 left-0 right-0'>{username.charAt(0)}</span>
           </div>
