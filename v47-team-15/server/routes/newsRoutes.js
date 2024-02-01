fadb18a344f94e16c182a5c707b1dd083829f485
Isabell <isabellhepburn@outlook.com>
2024-02-01 17:49:15 +0100

    fix:tidy up codes

--- v47-team-15/server/routes/newsRoutes.js
+++ v47-team-15/server/routes/newsRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const newsController = require("./newsController");
 
-//The fromDate parameter allows searching for articles up to one month old. For instance, if today is the 1st of February 2024, the earliest date for 'fromDate' should be 2024-01-01.
+//The fromDate parameter allows searching for articles up to one month old. 
+//For instance, if today is the 1st of February 2024, the earliest date for 'fromDate' should be 2024-01-01.
 
-// Example API requests:
+//Example API requests:
 //https://newsapi.org/v2/everything?q=crpto&from=$2024-01-05&sortBy=publishedAt&apiKey=${apiKey}&searchin=title,content
 
 router.get("/api/news", async (req, res) => {
