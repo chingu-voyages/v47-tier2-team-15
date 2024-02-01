3af0a265bedeb148f6c15e58a98bd175970cf7c5
Kseniia Riabova <kseniaistratova@gmail.com>
2024-02-01 10:58:29 +0100

    Add redirect to login
    -Added redirection to login

--- v47-team-15/server/middleware/authMiddleware.js
+++ v47-team-15/server/middleware/authMiddleware.js
@@ -24,6 +24,6 @@ module.exports.ensureAuthenticated = function (req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-
   res.status(401).send('Unauthorized');
+  res.redirect('/auth/login');
 };
