050d6bf56f981e2c83267b84e847c837626fe584
Ana Popovic <franjetina@gmail.com>
2024-02-01 14:03:33 +0100

    feat: style and functionality finished for user avatar

--- v47-team-15/client/src/components/user_profile/UserInfo.jsx
+++ v47-team-15/client/src/components/user_profile/UserInfo.jsx
@@ -10,88 +10,12 @@ function UserInfo() {
 
   return (
     <>
-      {/* <div className="bg-[#1A183E] text-white h-1/2 p-16">
-        <div className="w-full flex flex-row">
-          <div className="flex flex-col align-center text-center">
-            <i className="bx bxs-user text-[4rem]"></i>
-            <h1>{username}</h1>
+      <div className="w-full bg-[#1A183E] text-white h-1/2 p-16 flex flex-col justify-center items-center gap-4">
+        <div className='text-2xl'>Hello {username}!</div>
+        <div className="w-28 h-28 text-center rounded-full bg-[#24224B] uppercase relative">
+          <span className='text-3xl absolute top-8 left-0 right-0'>{username.charAt(0)}</span>
           </div>
-          <div className=''>
-          <Chart />
-          </div> */}
-      {/* <div className="w-full flex flex-row justify-center gap-6">
-            <div className="hidden md:flex flex-row items-center justify-center gap-8">
-              <div className="md:min-w-[10rem] lg:min-w-[14rem] bg-[#1D2A41] rounded py-4 px-10">
-                <p className="pb-4">Market Cap</p>
-                <div className="flex flex-row gap-4">
-                  <span>123$</span>
-                  <span className="rounded bg-[#1F3A43] px-1">nest</span>
-                </div>
-              </div>
-            </div>
-            <div className="hidden md:flex flex-row items-center justify-center gap-8">
-              <div className="md:min-w-[10rem] lg:min-w-[14rem] bg-[#1D2A41] rounded py-4 px-10">
-                <p className="pb-4">Market Cap</p>
-                <div className="flex flex-row gap-4">
-                  <span>123$</span>
-                  <span className="rounded bg-[#1F3A43] px-1">nest</span>
-                </div>
-              </div>
-            </div>
-            <div className="hidden md:flex flex-row items-center justify-center gap-8">
-              <div className="md:min-w-[10rem] lg:min-w-[14rem] bg-[#311B3D]  rounded py-4 px-10">
-                <p className="pb-4">Market Cap</p>
-                <div className="flex flex-row gap-4">
-                  <span>123$</span>
-                  <span className="rounded bg-[#461E3C] px-1">nest</span>
-                </div>
-              </div>
-            </div>
-          </div> */}
-      {/* </div>
-      </div> */}
-
-      <div className="w-full bg-[#1A183E] text-white h-1/2 p-16 flex flex-row px-32">
-        <div className="w-full bg-[#24224B] rounded flex flex-row align-center text-center gap-8 p-4">
-          {/* <i className="bx bxs-user text-[4rem]"></i> */}
-          <div className="w-1/3 flex flex-col justify-around items-center">
-            {/* <Chart className="" /> */}
-            <h1 className="text-[2rem]">{username}</h1>
-            <p className='bg-[#461E3C] py-4 w-1/2'>5 favorite coins </p>
-          </div>
-          <div className='w-full flex flex-row justify-around'>
-          <div className="hidden md:flex flex-row items-center justify-center gap-8">
-              <div className="md:min-w-[10rem] lg:min-w-[14rem] bg-[#1D2A41] rounded py-4 px-10">
-                <p className="pb-4">Bitcoin</p>
-                <div className="flex flex-row gap-4">
-                  <span>123456789$</span>
-                  <span className="rounded bg-[#1F3A43] px-1">15%</span>
-                </div>
-              </div>
-            </div>
-
-            <div className="hidden md:flex flex-row items-center justify-center gap-8">
-              <div className="md:min-w-[10rem] lg:min-w-[14rem] bg-[#1D2A41] rounded py-4 px-10">
-                <p className="pb-4">Ethereum</p>
-                <div className="flex flex-row gap-4">
-                  <span>123456789$</span>
-                  <span className="rounded bg-[#1F3A43] px-1">15%</span>
-                </div>
-              </div>
-            </div>
-
-            <div className="hidden md:flex flex-row items-center justify-center gap-8">
-              <div className="md:min-w-[10rem] lg:min-w-[14rem] bg-[#1D2A41] rounded py-4 px-10">
-                <p className="pb-4">Tether</p>
-                <div className="flex flex-row gap-4">
-                  <span>123456789$</span>
-                  <span className="rounded bg-[#1F3A43] px-1">15%</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* <Chart2 /> */}
-        </div>
+        <small>Your portfolio includes favourites</small>
       </div>
     </>
   );
