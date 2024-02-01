5a182cf25763bd21a7f971eca172221c10656866
117Isabell <isabellhepburn@outlook.com>
2024-02-02 01:26:02 +0800

    feat:import newsRoute in server

--- v47-team-15/server/server.js
+++ v47-team-15/server/server.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const passport = require('passport');
-const authRoutes = require('./routes/authRoute');
-const currenciesRoute = require('./routes/currenciesRoute');
-const globalRoute = require('./routes/globalRoute');
-const profileRoute = require('./routes/profileRoute');
-const { errorHandler } = require('./middleware/errorMiddleware');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const authRoutes = require("./routes/authRoute");
+const currenciesRoute = require("./routes/currenciesRoute");
+const globalRoute = require("./routes/globalRoute");
+const profileRoute = require("./routes/profileRoute");
+const newsRoute = require('("./routes/newsRoute"');
+const { errorHandler } = require("./middleware/errorMiddleware");
+require("dotenv").config();
 
 const app = express();
 
 mongoose
   .connect(process.env.MONGO_CONNECTION)
-  .then(() => console.log('Database connected! WIIIIIIII'))
+  .then(() => console.log("Database connected! WIIIIIIII"))
   .catch((err) => console.log(err));
 
-require('./passport/passport-config');
+require("./passport/passport-config");
 
 app.use(
   session({
@@ -37,17 +38,17 @@ app.use(express.json());
 // CORS
 app.use(
   cors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
 );
 
 // Routes
-app.use('/auth', authRoutes);
-app.use('/api/currencies', currenciesRoute);
-app.use('/api/global', globalRoute);
-app.use('/profile', profileRoute);
+app.use("/auth", authRoutes);
+app.use("/api/currencies", currenciesRoute);
+app.use("/api/global", globalRoute);
+app.use("/profile", profileRoute);
 
 app.use(errorHandler);
 
