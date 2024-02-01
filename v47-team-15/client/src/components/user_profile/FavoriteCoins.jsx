7f8c19f27e32250440efddc5dfb1a2f64a49f3c7
Ana Popovic <franjetina@gmail.com>
2024-02-01 14:05:51 +0100

    feat: fav coins section styled

--- v47-team-15/client/src/components/user_profile/FavoriteCoins.jsx
+++ v47-team-15/client/src/components/user_profile/FavoriteCoins.jsx
@@ -1,33 +1,38 @@
-import Chart from "../Chart"
-import FavoriteTable from "../user_profile/FaboriteTable"
-
 function FavoriteCoins() {
   return (
     <>
-        <div className="w-full bg-[#1A183E] px-32">
-            <div className="flex flex-row">
-            <div className="w-1/3 flex flex-col bg-[#24224B] p-6">
-            <Chart />
-            <div className="w-full text-white">
-                <p>
-                    <span className="">Bitcoin</span>
-                    <span className="">123456$</span>
-                </p>
-                <p>
-                    <span className="">Ethereum</span>
-                    <span className="">123456$</span>
-                </p>
-                <p className="w-full px-4">
-                    <span className="text-left">Tether</span>
-                    <span className="text-right">123456$</span>
-                </p>
+      <div className="w-full bg-[#1A183E] pb-8">
+        <div className=" bg-[#24224B] text-white py-10">
+          <h1 className="text-center text-3xl pb-4">My Favorite Coins</h1>
+          <p className="text-center text-sm pb-6">
+            View and manage your favorite cryptocurrencies.
+          </p>
+          <div className="flex flex-row justify-around items-center gap-4 pt-4">
+            <div className="flex flex-col items-center">
+              <i className="bx bx-star text-yellow-500 text-2xl"></i>
+              <p>Bitcoin</p>
             </div>
+            <div className="flex flex-col items-center">
+              <i className="bx bx-star text-yellow-500 text-2xl"></i>
+              <p>Bitcoin</p>
             </div>
-            <FavoriteTable />
+            <div className="flex flex-col items-center">
+              <i className="bx bx-star text-yellow-500 text-2xl"></i>
+              <p>Bitcoin</p>
             </div>
+            <div className="flex flex-col items-center">
+              <i className="bx bx-star text-yellow-500 text-2xl"></i>
+              <p>Bitcoin</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <i className="bx bx-star text-yellow-500 text-2xl"></i>
+              <p>Bitcoin</p>
+            </div>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default FavoriteCoins
+export default FavoriteCoins;
