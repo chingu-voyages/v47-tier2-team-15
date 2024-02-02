98bc0ee3103bad7f57cc854f56abdc99a5cf59c2
Ana Popovic <franjetina@gmail.com>
2024-02-02 13:20:48 +0100

    feat: new hook added to fetch user profile data

---
+++ v47-team-15/client/src/components/useUserProfile.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useUserProfile = () => {
+  const [favoriteCoins, setFavoriteCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const response = await axios.get('http://localhost:3003/profile', {
+          withCredentials: true,
+          responseType: 'json',
+        });
+        const userProfileData = response.data;
+
+        setFavoriteCoins(userProfileData.favoriteCoinsDetails);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchUserProfile();
+  }, []);
+
+  return { favoriteCoins, isLoading, error };
+};
+
+export default useUserProfile;
