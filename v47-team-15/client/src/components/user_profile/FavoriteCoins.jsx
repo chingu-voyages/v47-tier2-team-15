2c9d22084703001eadb8d5ab2e63ed3c2cbb4c41
Ana Popovic <franjetina@gmail.com>
2024-02-02 13:20:25 +0100

    feat: display 5 coins on page

--- v47-team-15/client/src/components/user_profile/FavoriteCoins.jsx
+++ v47-team-15/client/src/components/user_profile/FavoriteCoins.jsx
@@ -1,33 +1,30 @@
+import useUserProfile from '../useUserProfile';
+
 function FavoriteCoins() {
+  const { favoriteCoins } = useUserProfile();
+  console.log('fav coins', favoriteCoins);
+
+  const sortedCoins = favoriteCoins.slice().sort((a, b) => b.price - a.price);
+
+  const top5Coins = sortedCoins.slice(0, 5);
+
   return (
     <>
       <div className="w-full bg-[#1A183E] pb-8">
-        <div className=" bg-[#24224B] text-white py-10">
+        <div className="bg-[#24224B] text-white py-10">
           <h1 className="text-center text-3xl pb-4">My Favorite Coins</h1>
           <p className="text-center text-sm pb-6">
             View and manage your favorite cryptocurrencies.
           </p>
           <div className="flex flex-row justify-around items-center gap-4 pt-4">
-            <div className="flex flex-col items-center">
-              <i className="bx bx-star text-yellow-500 text-2xl"></i>
-              <p>Bitcoin</p>
-            </div>
-            <div className="flex flex-col items-center">
-              <i className="bx bx-star text-yellow-500 text-2xl"></i>
-              <p>Bitcoin</p>
-            </div>
-            <div className="flex flex-col items-center">
-              <i className="bx bx-star text-yellow-500 text-2xl"></i>
-              <p>Bitcoin</p>
-            </div>
-            <div className="flex flex-col items-center">
-              <i className="bx bx-star text-yellow-500 text-2xl"></i>
-              <p>Bitcoin</p>
-            </div>
-            <div className="flex flex-col items-center">
-              <i className="bx bx-star text-yellow-500 text-2xl"></i>
-              <p>Bitcoin</p>
-            </div>
+            {top5Coins.map((coin) => (
+              <div className="flex flex-col items-center" key={coin.id}>
+                <i className="bx bx-star text-yellow-500 text-2xl"></i>
+                <p>{coin.name}</p>
+                <p>{coin.symbol}</p>
+                <p>{coin.price}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
