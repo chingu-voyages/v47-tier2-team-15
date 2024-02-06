ad3253d65a1d37aee972f4d06bedf60732eb9260
Ana Popovic <franjetina@gmail.com>
2024-02-06 10:01:45 +0100

    fix: news cards fixed

--- v47-team-15/client/src/components/useNews.jsx
+++ v47-team-15/client/src/components/useNews.jsx
@@ -3,25 +3,31 @@ import axios from 'axios';
 
 function useNews() {
     const [newsData, setNewsData] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
   
     useEffect(() => {
       const fetchNews = async () => {
         try {
+          setIsLoading(true);
           const response = await axios.get('http://localhost:3003/api/news', {
             withCredentials: true,
             responseType: 'json',
           });
+          setIsLoading(false)
           setNewsData(response.data.articles);
           console.log(response.data.articles);
         } catch (error) {
           console.error('Error fetching news data:', error.message);
+          setError(error);
+          setIsLoading(false);
         }
       };
   
       fetchNews();
     }, []); 
 
-    return { newsData }
+    return { newsData, isLoading, error }
 }
 
 
