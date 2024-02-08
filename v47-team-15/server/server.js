a12576cc76b1ca90fa04bb81fee9feca5ac3de4e
Ana Popovic <franjetina@gmail.com>
2024-02-08 12:52:38 +0100

    configure cors

--- v47-team-15/server/server.js
+++ v47-team-15/server/server.js
@@ -12,9 +12,11 @@ app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://65c4bca05f631d0ad181816b--jazzy-hotteok-614a27.netlify.app/',
+  origin: 'https://65c4bca05f631d0ad181816b--jazzy-hotteok-614a27.netlify.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
+  allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+  exposedHeaders: 'Access-Control-Allow-Origin,Access-Control-Allow-Credentials',
 }));
 
 app.use('/api/currencies', currenciesRoute);
