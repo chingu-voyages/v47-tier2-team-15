6eccf84d552c53ce265deef0adcfc4a31213a9b5
Ana Popovic <franjetina@gmail.com>
2024-02-13 15:23:16 +0100

    endpoint added

--- v47-team-15/client/src/components/display_data/InfoCards.jsx
+++ v47-team-15/client/src/components/display_data/InfoCards.jsx
@@ -3,7 +3,7 @@ import { formatNumber, getColor } from '../Helpers';
 
 
 function InfoCards() {
-  const { data } = useFetch('/api/global');
+  const { data } = useFetch('https://cryptoview-us13.onrender.com//api/global');
 
   if (!data || !data[0]) {
     return <div>No data available</div>;
