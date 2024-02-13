0913c9571a6a65032649596af66e1a0b5bf6e076
Ana Popovic <franjetina@gmail.com>
2024-02-13 15:41:53 +0100

    code cleanup

--- v47-team-15/client/src/components/useFetch.jsx
+++ v47-team-15/client/src/components/useFetch.jsx
@@ -14,6 +14,7 @@ function useFetch(url) {
         const response = await axios.get(url, {
           timeout: 15000,
           withCredentials: true,
+          responseType: 'json',
         });
 
         const responseData = response.data.data;
