534b85c62a9c166982d7da0a7aa00d5c7b4fd7ec
Kseniia Riabova <kseniaistratova@gmail.com>
2024-02-16 19:07:07 +0100

    Updated passport-config
    Commented out trouble shooting code in passport-config.js

--- v47-team-15/server/passport/passport-config.js
+++ v47-team-15/server/passport/passport-config.js
@@ -1,7 +1,7 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt");
-const User = require("../models/user");
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
 /**
  * Configure Passport to use a local strategy for authentication.
@@ -11,25 +11,25 @@ const User = require("../models/user");
  */
 passport.use(
   new LocalStrategy(
-    { usernameField: "email" },
+    { usernameField: 'email' },
     async (email, password, done) => {
       try {
         const user = await User.findOne({ email });
 
         if (!user) {
-          return done(null, false, { message: "Incorrect email." });
+          return done(null, false, { message: 'Incorrect email.' });
         }
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         //Troubleshooting
-        console.log("Entered Email:", email);
-        console.log("Entered Password:", password);
-        console.log("Hashed Password from DB:", user.password);
-        console.log("Password Comparison Result:", isPasswordValid);
+        // console.log("Entered Email:", email);
+        // console.log("Entered Password:", password);
+        // console.log("Hashed Password from DB:", user.password);
+        // console.log("Password Comparison Result:", isPasswordValid);
 
         if (!isPasswordValid) {
-          return done(null, false, { message: "Incorrect password." });
+          return done(null, false, { message: 'Incorrect password.' });
         }
 
         return done(null, user);
@@ -46,7 +46,7 @@ passport.use(
  * @param {Function} done - Callback function to indicate completion.
  */
 passport.serializeUser((user, done) => {
-  console.log("Serializing user:", user);
+  console.log('Serializing user:', user);
   done(null, user.id);
 });
 
@@ -59,10 +59,10 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
-    console.log("Deserializing user:", user);
+    console.log('Deserializing user:', user);
     done(null, user);
   } catch (error) {
-    console.error("Deserialization error:", error.message);
+    console.error('Deserialization error:', error.message);
     done(error);
   }
 });
